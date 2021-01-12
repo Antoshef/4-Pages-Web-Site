4d9f12cd99fb57c6cd944f5d326515b17f2864ab
Anton Stanev <antoshef21@gmail.com>
2021-01-12 21:55:47 +0200

    Web

--- teachers.js
+++ teachers.js
@@ -15,12 +15,22 @@ solve()
 function burger() {
     let navElements = document.getElementsByClassName('c-header__nav')[0];
     if (navElements.className.includes('u-block')) {
-        navElements.className = 'c-header__nav';
+        navElements.classList.remove('u-block');
     } else {
-        navElements.className += ' u-block';
+        navElements.classList.add('u-block');
     };
 };
 
-function slider() {
-    console.log('fqwdqw');
-};
+// let parentsFeedBack = document.getElementsByClassName('c-skills__feedback-nav');
+// let i = 0;
+
+// function sliderLeft() {
+//     let current = parentsFeedBack[i]
+//     current.classList.add('a-slide-left');
+//     i++;
+//     if (i > 2) {i = 0};
+// };
+
+// function sliderRight() {
+    
+// };
